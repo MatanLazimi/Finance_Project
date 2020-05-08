ca5382defef7e54272920f7499e4632bf20a54f6
MatanLazimi <matanlazimi@gmail.com>
2020-05-08 14:03:54 +0300

    years correction

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main():
         cell_obj1 = sheet_obj.cell(row=k, column=6)
         cell_obj2 = sheet_obj.cell(row=k, column=12)
         if cell_obj2.value is None or cell_obj2.value == "-" :
-            print(int((date.today() - cell_obj1.value.date()).days/365),end=" ")
+            print(int((date.today() - cell_obj1.value.date()).days/365.25),end=" ")
         else:
-            print(int((cell_obj2.value.date() - cell_obj1.value.date()).days/365),end=" ")
+            print(int((cell_obj2.value.date() - cell_obj1.value.date()).days/365.25),end=" ")
         print()
 ######################################################################
 
